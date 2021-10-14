56f2e4f64c165ebb0a389dd84bdf4731f8bf07c4
Hector Miranda <hector.miranda8@upr.edu>
2021-10-13 20:49:16 -0400

    Student
    Implemented Student, missing proper drop due to splicing vector

--- course_mod.rs
+++ course_mod.rs
@@ -6,6 +6,28 @@ pub struct Course {
     grades: Grades
 }
 
+impl Course {
+    pub fn new (code: String) -> Course {
+        Course {
+            code: code,
+            grade_avg: 0.0,
+            grades: Grades::with_capacity(100)
+        }
+    }
+
+    pub fn get_code (&self) -> String {
+        return self.code;
+    }
+
+    pub fn get_grade_avg(&self) -> f32 {
+        return self.grade_avg;
+    }
+
+    pub fn get_grades(&self) -> Grades {
+        return self.grades;
+    }
+}
+
 pub fn valid_Code(code: String) -> bool{
     // code must be length 8
     if code.len() != 8 {
@@ -15,8 +37,8 @@ pub fn valid_Code(code: String) -> bool{
     // first 4 must be characters
     for n in 0..4 {
         let mut ch: char = code.as_bytes()[n] as char;
-        if ((ch >= 'a' && ch <= 'z') || 
-            (ch >= 'A' && ch <= 'Z')) {
+        if (ch >= 'a' && ch <= 'z') || 
+            (ch >= 'A' && ch <= 'Z') {
             // pass
         } else {
             return false;
@@ -25,7 +47,7 @@ pub fn valid_Code(code: String) -> bool{
     // last 4 must be digits
     for n in 4..8 {
         let mut ch: char = code.as_bytes()[n] as char;
-        if ((ch >= '0' && ch <= '9')) {
+        if ch >= '0' && ch <= '9' {
             // pass
         } else {
             return false;
--- student_mod.rs
+++ student_mod.rs
@@ -1,23 +1,107 @@
 use course_mod::Course;
+mod course_mod;
 
 type Courses = Vec<Course>;
 
-pub struct Student {
+struct Student_struct {
     name: String,
     date_of_birth: u32,
     grade_avg: f32,
     courses: Courses
 }
 
-pub fn get_student_average(courses: Courses) {
-    let len_courses = courses.len();
-    let mut avg: f32 = 0.0;
+trait new {
+    fn new(name: String, date_of_birth: u32) -> Self;
+}
 
-    for c in courses {
-        avg += c.grade_avg;
+trait enroll {
+    fn enroll(&self, code: String) -> bool;
+}
+
+trait drop {
+    fn drop(&self, code: String) -> bool;
+}
+
+trait to_string {
+
+}
+
+impl Student_struct {
+    fn calculate_average(&mut self) {
+        let mut len_courses = self.courses.len() as f32;
+        let mut avg: f32 = 0.0;
+    
+        for c in self.courses {
+            if c.get_grades().len() == 0 {
+                len_courses -= 1.0;
+            }
+            else {
+                avg += c.get_grade_avg();
+            }
+        }
+    
+        self.grade_avg = avg / len_courses;
     }
 
-    return avg / len_courses;
+    pub fn get_name(&self) -> String {
+        return self.name;
+    }
+
+    pub fn get_date_of_birth(&self) -> u32 {
+        return self.date_of_birth;
+    }
+
+    pub fn get_grade_avg(&self) -> f32 {
+        return self.grade_avg;
+    }
+
+    pub fn get_courses(&self) -> Courses {
+        return self.courses;
+    }
+}
+
+impl new for Student_struct {
+    fn new(name: String, date_of_birth: u32) -> Student_struct {
+        Student_struct {
+            name: name,
+            date_of_birth: date_of_birth,
+            grade_avg: 0.0,
+            courses: Courses::new()
+        }
+    }
+}
+
+impl enroll for Student_struct {
+    fn enroll(&self, code: String) -> bool {
+        if course_mod::valid_Code(code) {
+            let mut newCourse: Course = Course::new(code);
+            self.courses.push(newCourse);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
+
+impl drop for Student_struct {
+    fn drop(&self, code: String) -> bool {
+        if course_mod::valid_Code(code) {
+            let mut idx = 0;
+            for c in self.courses {
+                if c.get_code() == code {
+                    self.courses = TODO(SPLICE);
+                    return true;
+                }
+                idx += 1;
+            }
+            return false;
+        }
+        else {
+            return false;
+        }
+    }
 }
 
 fn main() {
